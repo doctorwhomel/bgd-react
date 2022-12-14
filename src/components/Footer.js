2a9ce52fdd1152706573dd851c239dd1af7f51b1
WHOMEL <williamhomel@gmail.com>
2022-12-14 11:55:45 -0800

    footerLinks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,31 +6,80 @@ const Footer = () => {
     <footer className="site-footer">
       <Container>
         <Row>
-          <Col xs={{ size: 4 }} sm="2">
+          <Col sm="4">
             <h5 className="nav-label">Links</h5>
             <ul className="list-unstyled">
               <li>
-                <Link
-                  to="/"
-                  style={{
-                    color: "gray",
-                    textDecoration: "none",
-                    fontFamily: "'Play', sans-serif",
-                  }}
-                >
+                <Link className="footerLink" to="/">
                   Home
                 </Link>
               </li>
               <li>
-                <Link to="/">Directory</Link>
+                <Link className="footerLink" to="/games">
+                  Games
+                </Link>
               </li>
               <li>
-                <Link to="/">About</Link>
+                <Link className="footerLink" to="/studios    ">
+                  Studios
+                </Link>
               </li>
               <li>
-                <Link to="/">Contact</Link>
+                <Link className="footerLink" to="/artists    ">
+                  Artists
+                </Link>
+              </li>
+              <li>
+                <Link className="footerLink" to="/about">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link className="footerLink" to="/contact">
+                  Contact
+                </Link>
               </li>
             </ul>
+          </Col>
+          <Col sm="4" className="text-center">
+            <h5 className="nav-label">Social</h5>
+            <a
+              className="btn btn-social-icon btn-instagram"
+              href="http://instagram.com/"
+            >
+              <i className="fa fa-instagram" />
+            </a>{" "}
+            <a
+              className="btn btn-social-icon btn-facebook"
+              href="http://www.facebook.com/"
+            >
+              <i className="fa fa-facebook" />
+            </a>{" "}
+            <a
+              className="btn btn-social-icon btn-twitter"
+              href="http://twitter.com/"
+            >
+              <i className="fa fa-twitter" />
+            </a>{" "}
+            <a
+              className="btn btn-social-icon btn-google"
+              href="http://youtube.com/"
+            >
+              <i className="fa fa-youtube" />
+            </a>
+          </Col>
+          <Col sm="4" className="text-center">
+            <a role="button" className="footerLink" href="tel:+13607391352">
+              <i className="fa fa-phone" /> 360-739-1352
+            </a>
+            <br />
+            <a
+              role="button"
+              className="footerLink"
+              href="mailto:williamhomel@gmail.com"
+            >
+              <i className="fa fa-envelope-o" /> williamhomel@gmail.com
+            </a>
           </Col>
         </Row>
       </Container>
